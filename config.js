ea3d8c1a4eec36651eab8d1e9543306a18c1ba7b
SL-BINUWA <141151972+SL-BINUWA@users.noreply.github.com>
2025-06-02 21:41:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=y55GDAqZ#MKGPYEuB0zDYHeDFQBw5y3GifQ4m1z9Y50Ermvt19rE' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
